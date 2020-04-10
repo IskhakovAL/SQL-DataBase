7381717a620d64e259095251009047b4a7fd1698
Askar Iskhakov <iskhakovaskar2017@gmail.com>
2020-04-10 21:33:03 +0300

    Add file creation_of_authentication_table.sql
    Add script for creation of authentication table.

---
+++ creation_of_authentication_table.sql
@@ -0,0 +1,23 @@
+/* Удаление таблицы Authentication */
+DROP TABLE Authentication;
+
+/* Создание таблицы Authentication */
+CREATE TABLE Authentication (
+	user_id INT NOT NULL,
+	login VARCHAR(40) UNIQUE NOT NULL,
+	password VARCHAR(40) NOT NULL,
+	date_of_creation TIMESTAMP NOT NULL DEFAULT NOW(),
+	deleted boolean DEFAULT FALSE,
+	PRIMARY KEY (user_id),
+	CONSTRAINT authentication_user_id_fkey FOREIGN KEY (user_id) REFERENCES Users (id)
+);
+
+/* Заполнение таблицы Authentication */
+INSERT INTO Authentication
+	(user_id, login, password)
+VALUES
+	(1, 'Admin518_login', 'Admin518_password'),
+	(2, 'User518_login', 'User518_password');
+
+/* Вывод таблицы Authentication */
+SELECT * FROM Authentication;
