042c5d4c8a1d959f1c452a82438b2fc991599a86
Askar Iskhakov <iskhakovaskar2017@gmail.com>
2020-04-11 18:09:13 +0300

    Add file creation_of_users_roles_table.sql
    Add script for creation of users roles table.

---
+++ creation_of_users_roles_table.sql
@@ -0,0 +1,25 @@
+/* Удаление таблицы Users_Roles */
+DROP TABLE Users_Roles;
+
+/* Создание таблицы Users_Roles */
+CREATE TABLE Users_Roles (
+	user_id INT NOT NULL,
+	role_id INT NOT NULL,
+	date_of_creation TIMESTAMP NOT NULL DEFAULT NOW(),
+	deleted boolean DEFAULT FALSE,
+	PRIMARY KEY (user_id, role_id),
+	CONSTRAINT users_roles_user_id_fkey FOREIGN KEY (user_id) REFERENCES Users (id)
+	MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION,
+	CONSTRAINT users_roles_role_id_fkey FOREIGN KEY (role_id) REFERENCES Roles (id)
+	MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION
+);
+
+/* Заполнение таблицы Users_Roles */
+INSERT INTO Users_Roles
+	(user_id, role_id)
+VALUES
+	(1, 1),
+	(2, 2);
+
+/* Вывод таблицы Users_Roles */
+SELECT * FROM Users_Roles;
