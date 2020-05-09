5b0745c0d370f5558c6f0fe82538aad964692873
Askar Iskhakov <iskhakovaskar2017@gmail.com>
2020-05-10 00:03:58 +0300

    Add view.

--- creation_of_roles_privileges_descriptions_view.sql
+++ creation_of_roles_privileges_descriptions_view.sql
@@ -4,10 +4,10 @@ DROP VIEW Roles_Privileges_Descriptions;
 /* Создание представления Roles_Privileges_Descriptions */
 CREATE VIEW Roles_Privileges_Descriptions AS
 SELECT
-	Roles.id,
-	Roles.name,
-	Roles.description,
-	array_agg(DISTINCT Privileges.id ORDER BY Privileges.id)
+	Roles.id AS role_id,
+	Roles.name AS role_name,
+	Roles.description AS role_description,
+	array_agg(DISTINCT Privileges.id ORDER BY Privileges.id) AS role_privileges
 FROM Roles
 LEFT JOIN Roles_Privileges on Roles.id=Roles_Privileges.role_id
 LEFT JOIN Privileges on Roles_Privileges.privilege_id=Privileges.id
